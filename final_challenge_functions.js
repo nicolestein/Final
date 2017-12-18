600df3c9236c15f995ca0bac33ab702dc9fe2751
Nicole Stein <nstein@Command-Center.local>
2017-12-18 09:54:06 -0800

    Work

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -1,8 +1,30 @@
 function checkPrime(number) {
-    for (i=2, i<=number/2, i++) {
-        if (number%i=0) {
-            return "not prime";
+    if (number % 1 != 0 || number <= 0){
+        return "Not Prime";
+
+    }else if (number>4){
+        for (var i = 2; i <= number/2; i++){
+            if (number % 1 == 0 || number % i == 0) {
+                return "not prime";
+            }
+        }
+        return "prime";
+    } else {
+        return "prime";
     }
 }
-return "prime";
+
+
+function table(n) {
+    var rows = n;
+    var cells = n;
+    document.write("<table>");
+    for (i = 1; i <= rows; i++) {
+        document.write("<tr>");
+        for (j = 1; j <= cells; j++) {
+            document.write("<td>", i * j, "<td>");
+        }
+        document.write("</tr>");
+    }
+    return ("</tables>")
 }
